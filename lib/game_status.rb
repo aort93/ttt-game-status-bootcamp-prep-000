e72e2856ff0e6f7d7ae9d3ec6784a0ce05afee98
Alonso Ortega <alonsojortega@gmail.com>
2017-09-26 04:28:52 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,6 +4,7 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+=begin
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
@@ -40,3 +41,4 @@ def winner(board)
     board[winning_combo.first]
   end
 end
+=end
